06805a0a18fefcb54b3380fcb54239b81a224f4e
Your Name <you@example.com>
2024-05-13 02:28:13 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -77,21 +77,20 @@ void eraseTreeMap(TreeMap * tree, void* key){
 
 
 
-
-Pair *searchTreeMap(TreeMap *tree, void *key) {
-    TreeNode *current = tree->root;
+Pair *searchTreeMap(TreeMap *tree, void *key, TreeNode **current) {
+    *current = tree->root;
 
     while (current != NULL) {
-        int cmp = tree->lower_than(key, current->pair->key);
+        int cmp = tree->lower_than(key, (*current)->pair->key);
         if (cmp == 0) {
 
-            return current->pair;
+            return (*current)->pair;
         } else if (cmp < 0) {
 
-            current = current->left;
+            current = (*current)->left;
         } else {
 
-            current = current->right;
+            current = (*current)->right;
         }
     }
     return NULL;
