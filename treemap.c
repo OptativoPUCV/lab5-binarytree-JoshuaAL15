90169855894a59954a109f9565aaaad84f87c450
Your Name <you@example.com>
2024-05-19 03:08:48 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -20,11 +20,6 @@ struct TreeMap {
     int size;
 };
 
-int is_equal(TreeMap* tree, void* key1, void* key2){
-    if(tree->lower_than(key1,key2)==0 &&  
-        tree->lower_than(key2,key1)==0) return 1;
-    else return 0;
-}
 
 
 TreeNode * createTreeNode(void* key, void * value) {
